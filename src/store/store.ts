cf396874ae0423600c920b499bdd20cf0debe3a2
Quaan <quandev.fr@gmail.com>
2025-12-29 17:49:34 +0700

    feat: setup redux thunk

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import { rootReducer } from './rootReducer';
 
 export const store = configureStore({
-  reducer: {},
-})
+  reducer: rootReducer,
+  devTools: import.meta.env.Dev,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
